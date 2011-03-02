a3b009be406098a38755ef1466c86587946babde
Ilja Livenson <ilja.livenson@gmail.com>
2011-03-02 22:44:49 +0200

    header parsing added

--- src/vcdm/server/cdmi/blob.py
+++ src/vcdm/server/cdmi/blob.py
@@ -1,5 +1,7 @@
 from twisted.web import resource, server
 from vcdm import blob
+from vcdm.server.cdmi.cdmi_content_types import CDMI_DATA, CDMI_OBJECT
+from vcdm.server.cdmi.root import CDMI_VERSION
 
 class Blob(resource.Resource):
     isLeaf = True # data items cannot be nested
@@ -7,12 +9,18 @@ class Blob(resource.Resource):
 
     def render_GET(self, request):
         # TODO: get fnm from request, validate. for now just assume it's everything after the path
-        fnm = '/' + request.postpath[0]
+        fnm = '/' + request.postpath[0]        
         # TODO: get range of bytes from the request. For now just assume static
-        rng = None #(1,4)
+        #rng = request.
+        rng = None
         status, d = blob.read(fnm, rng)
+        
+        # construct response
         request.setResponseCode(status)
-        return "%s" % d 
+        request.setHeader('Content-Type', CDMI_OBJECT)
+        request.setHeader('X-CDMI-Specification-Version', CDMI_VERSION)
+        request.setHeader('Accept', CDMI_DATA)
+        return "%s" % d
 
     def render_PUT(self, request):
         # TODO: once we have a normal folder system, remove this hack
--- src/vcdm/server/cdmi/root.py
+++ src/vcdm/server/cdmi/root.py
@@ -1,20 +1,27 @@
 from twisted.web import resource, http
 
 import blob, queue
+from cdmi_content_types import CDMI_DATA, CDMI_QUEUE
+from vcdm.server.cdmi.cdmi_exit_codes import BAD_REQUEST
 
-cdmi_objects = {'mq': queue.Queue,
-                'blob': blob.Blob}
+cdmi_objects = {CDMI_QUEUE: queue.Queue,
+                CDMI_DATA: blob.Blob}
+
+CDMI_VERSION = '1.0'
 
 class RootCDMIResource(resource.Resource):
     """
     A root resource which is protected by guard and requires authentication in order to access.
     """
-    def getChild(self, type, request):       
-        # TODO: choose processor based on the mime type 
-        if type in cdmi_objects:
-            return cdmi_objects[type]()
-        else:
-            return self
+    def getChild(self, path, request):     
+        content = request.getHeader('content-type')
+        accept_field = request.getHeader('accept')
+        version = request.getHeader('x-cdmi-specification-version')
+        ## Request validation. 
+        # TODO: possibly move to a separate function            
+        if content != accept_field or CDMI_VERSION not in version or content not in cdmi_objects.keys():
+            return BAD_REQUEST               
+        return cdmi_objects[content]()            
 
     def render(self, request):
-        return "Unsupported"
+        return "Unforeseen error for request: %s", request
