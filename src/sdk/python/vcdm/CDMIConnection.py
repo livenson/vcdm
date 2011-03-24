a542af22b624598e25ebfe867257ea16b61d7fd5
unknown <ilja@.(none)>
2011-03-24 10:30:01 +0100

    mock functions for python sdk

--- src/sdk/python/vcdm/CDMIConnection.py
+++ src/sdk/python/vcdm/CDMIConnection.py
@@ -1,17 +1,23 @@
+from BlobOperations import BlobOperations
+
 
 class CDMIConnection():
     
     credentials = None
     endpoint = None
+    blob_proxy = None
+    container_proxy = None
+    mq_proxy = None
     
     def __init__(self, credentials, endpoint):
         self.credentials = credentials
         self.endpoint = endpoint
+        self.blob_proxy = BlobOperations(credentials, endpoint)
         
         
     def create_blob(self, remotefile, localfile,  mimetype=None, metadata=None):
         """Create a new blob from a local file"""
-        pass
+        
     
     def update_blob(self, remoteblob, localfile = None, mimetype=None, metadata=None):
         """Update a remote blob with new data."""
