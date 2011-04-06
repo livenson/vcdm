02d5720d9b60e700864f61118e966c81687afb38
Ilja Livenson <ilja.livenson@gmail.com>
2011-04-06 14:57:34 +0200

    inbetween step for queue implementation

--- src/vcdm/server/cdmi/queue.py
+++ src/vcdm/server/cdmi/queue.py
@@ -4,15 +4,42 @@ import cgi
 
 from vcdm import mq
 
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+from vcdm.server.cdmi.generic import set_common_headers, parse_path,\
+    get_common_body
+from vcdm.server.cdmi.cdmi_content_types import CDMI_QUEUE
+
 # TODO: refactor 
 class Queue(resource.Resource):
     allowedMethods = ('PUT','GET','DELETE', 'POST') # commands we support for the data items
     isLeaf = True # queues cannot be nested
     
     def render_GET(self, request):
-        qnm = request.postpath[0]
-        val = mq.get(qnm)
-        return "queue %s - last value: %s" %(qnm, val)
+        # parse the request        
+        name, container_path, fullpath = parse_path(request.path)
+        
+        status, value, uid, metadata, mimetype = mq.get(fullpath)
+        # construct response
+        request.setResponseCode(status)
+        request.setHeader('Content-Type', CDMI_QUEUE)
+        set_common_headers(request)            
+        
+        # construct body
+        response_body = {
+                         'completionStatus': 'Complete',
+                         'mimetype': mimetype, 
+                         'metadata': metadata,
+                         'value': value,
+                         'queueValues': None,
+                         'capabilitiesURI': '/cdmi_capabilities/queue'
+                         }  
+        response_body.update(get_common_body(request, uid, fullpath))
+        return json.dumps(response_body)
+
         
     def render_PUT(self, request):
         qnm = request.postpath[0]
