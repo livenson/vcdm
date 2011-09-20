e806a7dd26a8ec69a420e12acaa75b7cb52b8d3b
Ilja Livenson <ilja.livenson@gmail.com>
2011-09-20 15:48:49 +0300

    added childrenrange to the container put operation

--- src/vcdm/server/cdmi/container.py
+++ src/vcdm/server/cdmi/container.py
@@ -56,21 +56,22 @@ class Container(resource.Resource):
         if 'metadata' in body:
             metadata = body['metadata']
         status, uid, children = container.create_or_update(self.avatarID, name, container_path, fullpath, metadata)
-        
+
         request.setResponseCode(status)
         request.setHeader('Content-Type', CDMI_CONTAINER)
         set_common_headers(request)
-         
+
         # and a body
         response_body = {
                          'completionStatus': 'Complete', 
                          'metadata': metadata,
-                         'children': children
-                         }  
+                         'children': children,
+                         'childrenrange': '' if len(children) == 0 else '0-%s' % len(children),
+                         }
         response_body.update(get_common_body(request, uid, fullpath))
-        
+
         return json.dumps(response_body)
-    
+
     def render_DELETE(self, request):
         _, __, fullpath = parse_path(request.path)
         status = container.delete(self.avatarID, fullpath)
@@ -78,10 +79,11 @@ class Container(resource.Resource):
         request.setHeader('Server', CDMI_SERVER_HEADER)   
         return ""
 
+
 class NonCDMIContainer(resource.Resource):
     isLeaf = True
     allowMethods = ('PUT', 'GET', 'DELETE')
-    
+
     def __init__(self, avatarID = None):        
         resource.Resource.__init__(self)
         self.avatarID = avatarID
@@ -90,14 +92,14 @@ class NonCDMIContainer(resource.Resource):
         """GET operation corresponds to reading a container's data."""
         # parse the request        
         _, __, fullpath = parse_path(request.path)
-        
+
         # contact the backend
         status, _, children, metadata = container.read(self.avatarID, fullpath)
-        
+
         # create a header
         request.setResponseCode(status)
         request.setHeader('Content-Type', 'application/json')
-        
+
         # and a body
         # this is not a completely correct implementation of a non-cdmi reply.
         # TODO: change to more specific fields once fields separation is implemented
@@ -105,18 +107,19 @@ class NonCDMIContainer(resource.Resource):
                          'metadata': metadata,
                          'children': children,
                          }
-        
+
         return json.dumps(response_body)
-    
+
     def render_PUT(self, request):
         name, container_path, fullpath = parse_path(request.path)
         status, _, __ = container.create_or_update(self.avatarID, name, container_path, fullpath, {})
         request.setResponseCode(status)
         return ""
-    
+
     def render_DELETE(self, request):
         _, __, fullpath = parse_path(request.path)
         status = container.delete(self.avatarID, fullpath)
         request.setResponseCode(status)
         request.setHeader('Server', CDMI_SERVER_HEADER)
         return ""
+
