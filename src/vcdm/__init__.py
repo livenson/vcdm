bbc5578824bc37db5918955f70eaccb92b2dbd95
Ilja Livenson <ilja.livenson@gmail.com>
2011-12-14 14:04:00 +0100

    removed obligatory addition of both md5 and plaintext passwords

--- src/vcdm/__init__.py
+++ src/vcdm/__init__.py
@@ -23,16 +23,16 @@ env = {'ds': None,
 # compulsory localdisk backend
 from vcdm.backends.blob.localdisk import POSIXBlob
 blob_backends = {'local': POSIXBlob}
- 
+
 # optional backends, some require presence of additional plugins
 try:
-    from vcdm.backends.blob.aws_s3 import S3Blob    
+    from vcdm.backends.blob.aws_s3 import S3Blob
     blob_backends['aws'] = S3Blob
 except ImportError:
     print "AWS back-end type not available"
 
 try:
-    from vcdm.backends.blob.azure import AzureBlob    
+    from vcdm.backends.blob.azure import AzureBlob
     blob_backends['azure'] = AzureBlob
 except ImportError:
     print "Azure back-end type not available"
@@ -41,27 +41,27 @@ try:
     from vcdm.backends.blob.cdmi import CDMIBlob
     blob_backends['cdmi'] = CDMIBlob
 except ImportError:
-    print "CDMI back-end type not available"            
+    print "CDMI back-end type not available"
 
 # mq is a non-compulsory object
 mq_backends = {}
 
 if c('general', 'support_mq') == 'yes' and c('general', 'mq.backend') == 'local':
     from vcdm.backends.mq.amqp import AMQPMQ
-    mq_backends['amqp'] = AMQPMQ                 
+    mq_backends['amqp'] = AMQPMQ
 
 if c('general', 'support_mq') == 'yes' and c('general', 'mq.backend') == 'cdmi':
     from vcdm.backends.mq.cdmi import CDMIQueue
-    mq_backends['cdmi'] = CDMIQueue                 
+    mq_backends['cdmi'] = CDMIQueue
 
-if c('general', 'support_mq') == 'yes' and c('general', 'mq.backend') == 'aws':    
+if c('general', 'support_mq') == 'yes' and c('general', 'mq.backend') == 'aws':
     from vcdm.backends.mq.aws_sqs import AWSSQSMessageQueue
-    mq_backends['aws'] = AWSSQSMessageQueue                 
+    mq_backends['aws'] = AWSSQSMessageQueue
 
-if c('general', 'support_mq') == 'yes' and c('general', 'mq.backend') == 'azure':    
+if c('general', 'support_mq') == 'yes' and c('general', 'mq.backend') == 'azure':
     from vcdm.backends.mq.azure import AzureQueue
     mq_backends['azure'] = AzureQueue
-             
+
 # datastore backends
 datastore_backends = {}
 if c('general', 'ds.backend') == 'couchdb':
--- src/vcdm/server.py
+++ src/vcdm/server.py
@@ -8,7 +8,6 @@ from twisted.cred.checkers import FilePasswordDB
 from twisted.internet import task
 
 import vcdm
-from vcdm import c
 from vcdm.server.cdmi.root import RootCDMIResource
 from vcdm.server.cdmi import current_capabilities
 from vcdm.utils import AccountingLogObserver
@@ -23,63 +22,63 @@ class SimpleRealm(object):
         if resource.IResource in interfaces:
             return resource.IResource, RootCDMIResource(avatarId), lambda: None
         raise NotImplementedError()
-    
+
 def main():
     # setup logging
-    log.startLogging(open(c('general', 'common_log'), 'a'), setStdout=False)
-    acclog = AccountingLogObserver(open(c('general', 'accounting_log'), 'a'))
+    log.startLogging(open(vcdm.config.get('general', 'common_log'), 'a'), setStdout=False)
+    acclog = AccountingLogObserver(open(vcdm.config.get('general', 'accounting_log'), 'a'))
     log.addObserver(acclog.emit)
 
     # initialize backends
-    vcdm.env['ds'] = vcdm.datastore_backends[c('general', 'ds.backend')]()
-    
+    vcdm.env['ds'] = vcdm.datastore_backends[vcdm.config.get('general', 'ds.backend')]()
+
     # load all backends
-    for blob_backend in c('general', 'blob.backends').split(','):
+    for blob_backend in vcdm.config.get('general', 'blob.backends').split(','):
         blob_backend = blob_backend.strip()
         log.msg("Activating %s backend." % blob_backend)
-        type = c(blob_backend, 'type')
-        vcdm.env['blobs'][blob_backend] = vcdm.blob_backends[type](blob_backend)
-    
+        backend_type = vcdm.config.get(blob_backend, 'type')
+        vcdm.env['blobs'][blob_backend] = vcdm.blob_backends[backend_type](blob_backend)
+
     # set default
-    vcdm.env['blob'] = vcdm.env['blobs'][c('general', 'blob.default.backend')]  
-    # initiate blob logging
-    task.LoopingCall(vcdm.blob.get_stored_size_all_avatars).start(float(c('general', 'accounting.total_frequency'))) #in seconds
+    vcdm.env['blob'] = vcdm.env['blobs'][vcdm.config.get('general', 'blob.default.backend')]
+    # initiate accounting logging
+    task.LoopingCall(vcdm.blob.get_stored_size_all_avatars). \
+                    start(float(vcdm.config.get('general', 'accounting.total_frequency'))) #in seconds
     
     # do we want queue backend? just a single one at the moment
-    if c('general', 'support_mq') == 'yes':
-        vcdm.env['mq'] = vcdm.mq_backends[c('general', 'mq.backend')]()
+    if vcdm.config.get('general', 'support_mq') == 'yes':
+        vcdm.env['mq'] = vcdm.mq_backends[vcdm.config.get('general', 'mq.backend')]()
         current_capabilities.system['queues'] = True
     # for now just a small list of 
     def _hash(name, clearpsw, hashedpsw):
         import hashlib
         return  hashlib.md5(clearpsw).hexdigest()
 
-    checkers = [FilePasswordDB(c('general', 'usersdb.plaintext'), cache=True), 
-                FilePasswordDB(c('general', 'usersdb.md5'), hash=_hash, cache=True)]
-    
+    checkers = []
+    if vcdm.config.has_option('general', 'usersdb.plaintext'):
+        checkers.append(FilePasswordDB(vcdm.config.get('general', 'usersdb.plaintext'), cache=True))
+
+    if vcdm.config.has_option('general', 'usersdb.md5'):
+        checkers.append(FilePasswordDB(vcdm.config.get('general', 'usersdb.md5'),
+                                                       hash=_hash, cache=True))
+
     wrapper = guard.HTTPAuthSessionWrapper(
         Portal(SimpleRealm(), checkers),
-        [guard.BasicCredentialFactory(c('general', 'server.endpoint')),
-         guard.DigestCredentialFactory('md5', c('general', 'server.endpoint'))])
-    
-    # TODO: configure reactor to use
-    # http://twistedmatrix.com/documents/current/core/howto/choosing-reactor.html
+        [guard.BasicCredentialFactory(vcdm.config.get('general', 'server.endpoint')),
+         guard.DigestCredentialFactory('md5', vcdm.config.get('general', 'server.endpoint'))])
     
     # unencrypted/unprotected connection for testing/development
-    if c('general', 'server.use_debug_port') == 'yes':      
-        reactor.listenTCP(int(c('general', 'server.debug_port')), server.Site(resource=RootCDMIResource()))
+    if vcdm.config.get('general', 'server.use_debug_port') == 'yes':
+        reactor.listenTCP(int(vcdm.config.get('general', 'server.debug_port')),
+                          server.Site(resource=RootCDMIResource()))
         reactor.listenTCP(2365, server.Site(resource=wrapper))
     
     # 1-way SSL for production
     from twisted.internet import ssl
     sslContext = ssl.DefaultOpenSSLContextFactory('server_credentials/key.pem','server_credentials/cert.pem')
-    reactor.listenSSL(int(c('general', 'server.endpoint').split(":")[1]), server.Site(resource=wrapper), contextFactory = sslContext)
-        
-    # connector for providing quick metainfo
-    #from vcdm.server.meta.info import InfoResource
-    # TODO: fix InfoResource
-    #reactor.listenTCP(8083, server.Site(resource = InfoResource()))
-    
+    reactor.listenSSL(int(vcdm.config.get('general', 'server.endpoint').split(":")[1]),
+                      server.Site(resource=wrapper), contextFactory = sslContext)
+
     reactor.run()
 
 if __name__ == '__main__':
