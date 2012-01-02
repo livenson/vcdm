6ee7351ebc123b4fee7f36bfe27de50404b2febf
Ilja Livenson <ilja.livenson@gmail.com>
2012-01-02 17:47:30 +0200

    cleanup

--- src/vcdm/backends/blob/aws_s3.py
+++ src/vcdm/backends/blob/aws_s3.py
@@ -13,17 +13,16 @@ from boto.exception import S3CreateError
 
 
 class S3Blob(IBlob):
-    
+
     conn = None
     backend_name = 's3'
-    
+
     def __init__(self, backend_name):
         self.backend_name = backend_name
-        self.conn = boto.connect_s3(c(backend_name, 'credentials.username'), 
-                                    c(backend_name, 'credentials.password'))    
+        self.conn = boto.connect_s3(c(backend_name, 'credentials.username'),
+                                    c(backend_name, 'credentials.password'))
         # create a new bucket - just in case
         self.cdmi_bucket_name = c(backend_name, 'aws.bucket_name')
-        
         # change to Location.USWest for the corresponding zone
         try:
             self.conn.create_bucket(self.cdmi_bucket_name, location = Location.EU)
@@ -41,7 +40,7 @@ class S3Blob(IBlob):
         k.get_contents_to_file(fp)
         fp.seek(0) # roll back to start
         return fp
-    
+
     def create(self, fnm, content):
         input_stream, _ = content
         b = self.conn.get_bucket(self.cdmi_bucket_name)
@@ -49,16 +48,16 @@ class S3Blob(IBlob):
         k.key = fnm
         k.set_contents_from_file(input_stream)
         input_stream.close()
-        
+
     def update(self, fnm, content):
         self.create(fnm, content)
-    
+
     def delete(self, fnm):
         b = self.conn.get_bucket(self.cdmi_bucket_name)
         k = Key(b)    
         k.key = fnm
         k.delete()
-        
+
     def move_to_tre_server(self, fnm):
         b = self.conn.get_bucket(self.cdmi_bucket_name)
         k = Key(b)    
@@ -68,10 +67,10 @@ class S3Blob(IBlob):
                                suffix=".buffer",
                                delete=False)        
         k.get_contents_to_file(fp)
-        
+
         source = os.path.join(c(self.backend_name, 'blob.datadir'), fp.name)
         target = os.path.join(c('general', 'tre_data_folder'), fnm)
-        
+
         try:
             os.symlink(os.path.abspath(source), os.path.abspath(target))
         except OSError, ex:
