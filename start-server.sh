eef5d30f3fa738101378efdc7c0fbcebab865bcb
Ilja Livenson <ilja.livenson@gmail.com>
2012-01-02 16:33:32 +0200

    updated submodules for backends

--- start-server.sh
+++ start-server.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-export PYTHONPATH="src:libsrc:$PYTHONPATH" 
+for lib in `ls libsrc`; do
+    if [ -e libsrc/$lib/src ]; then 
+        PYTHONPATH=libsrc/${lib}/src:$PYTHONPATH
+    else
+        PYTHONPATH=libsrc/${lib}:$PYTHONPATH
+    fi
+done
+PYTHONPATH=src:$PYTHONPATH
+export PYTHONPATH
+
 python src/vcdm/server.py
